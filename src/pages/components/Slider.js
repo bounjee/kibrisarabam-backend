c043f832467b3c4ae2b0d79b8a1f60ee7c0d3bab
bounjee <mustafaggww@gmail.com>
2024-05-10 02:21:46 +0300

    mesaj

--- src/pages/components/Slider.js
+++ src/pages/components/Slider.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
-import './Slider.css';
+import React, { useState, useRef } from 'react';
+import './Slider.css'
+
 
 const Slider = ({ images }) => {
     const [selectedImg, setSelectedImg] = useState(images[0]);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const thumbContainerRef = useRef(null);
+
+    const onMouseDown = (e) => {
+        setIsDragging(true);
+        setStartX(e.pageX - thumbContainerRef.current.offsetLeft); // Fare konumunu ayarlar
+    };
+
+    const onMouseMove = (e) => {
+        if (isDragging) {
+            e.preventDefault(); // Varsayılan olayı engeller
+            const x = e.pageX - thumbContainerRef.current.offsetLeft;
+            const walk = (x - startX) * 0.1; // Scroll hızını ayarlamak için çarpanı değiştirebilirsiniz
+            thumbContainerRef.current.scrollLeft -= walk;
+        }
+    };
+
+    const onMouseUp = () => {
+        setIsDragging(false);
+    };
 
     const renderThumbnails = () => {
         return images.map((img, index) => (
@@ -21,7 +43,12 @@ const Slider = ({ images }) => {
             <div className="main-img">
                 <img src={selectedImg} alt="Selected" className="fade-in" />
             </div>
-            <div className="thumb-container">
+            <div className="thumb-container" ref={thumbContainerRef}
+                 onMouseDown={onMouseDown}
+                 onMouseMove={onMouseMove}
+                 onMouseUp={onMouseUp}
+                 onMouseLeave={onMouseUp} // Fare konteynerdan çıkarsa kaydırmayı durdurur
+            >
                 {renderThumbnails()}
             </div>
         </div>
--- src/pages/otoDetails.js
+++ src/pages/otoDetails.js
@@ -4,21 +4,24 @@ import { ReactComponent as PhoneLogo } from '../assets/phone.svg';
 import { ReactComponent as WhatsappLogo } from '../assets/whatsapp.svg';
 import { ReactComponent as InstagramLogo } from '../assets/instagram.svg';
 import './otoDetails.css'
-import SliderComp from './components/Slider';  // Slider componenti
+import SliderComponent from '../pages/components/Slider'; // Yeni slider bileşeninin importu
 import Navbar from "./components/Navbar";
 
-import Image1 from '../assets/mainPhoto.jpg';
-import Image2 from '../assets/3278519.jpg';
-import Image3 from '../assets/3278520.jpg';
-import Image4 from '../assets/3278521.jpg';
+import mainPhoto from '../assets/mainPhoto.jpg';
+import Photo1 from   '../assets/3278519.jpg';
+import Photo2 from   '../assets/3278520.jpg';
+import Photo3 from   '../assets/3278521.jpg';
+import Photo4 from   '../assets/3278521.jpg';
+
+
 
 
 const images = [
-  Image1,
-  Image2,
-  Image3,
-  Image4
-
+  mainPhoto,
+  Photo1,
+  Photo2,
+  Photo3,
+  Photo4,
 ];
 
 
@@ -46,34 +49,60 @@ function otoDetails() {
         </div>
       </div> */}
 
-     {/* --------MAIN SECTION-------- */}
+    {/* --------MAIN SECTION-------- */}
     <div className='main_text'>car_name</div>
-    
-     <SliderComp images={images}/>
+
+    <SliderComponent images={images} />
 
     <div className='main-car_price-meeting_box-container'>
-      <div class="main-car_price">
-        <span>car_price</span>
-      </div> 
+      <button className="main-car_price">car_price</button> 
 
-      <div class="main-meeting-box">
-        <span>Randevu Oluştur</span>
-      </div> 
+      <button className="main-meeting-box">Randevu Oluştur</button> 
     </div>
 
     <div className='main-car_information'><p>Araç Bilgisi</p></div>
-    <ul className='main-car_information_texts'>
-      <li>Model Yılı: </li>
-      <li>Kilometre: </li>
-      <li>Motor: </li>
-      <li>Vites: </li>
-      <li>Yakıt Tüketimi: </li>
-      <li>0-100 km/h hızlanma: </li>
-      <li>Motor Gücü: </li>
-      <li>Araç Ağırlığı: </li>
-      <li>Bagaj Hacmi: </li>
-      <li>Araç Aktarımı: </li>
-    </ul>
+    <div className="main-car_information_texts">
+      <div className="attribute">
+        <span className="label">Model Yılı:</span>
+        <span className="value">model_year</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Kilometre:</span>
+        <span className="value">model_km</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Motor:</span>
+        <span className="value">model_motor</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Vites:</span>
+        <span className="value">model_gear</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Yakıt Tüketimi:</span>
+        <span className="value">model_fuel</span>
+      </div>
+      <div className="attribute">
+        <span className="label">0-100 km/h hızlanma:</span>
+        <span className="value">model_speed</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Motor Gücü:</span>
+        <span className="value">model_motor_power</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Araç Ağırlığı:</span>
+        <span className="value">model_carweight</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Bagaj Hacmi:</span>
+        <span className="value">model_luggage</span>
+      </div>
+      <div className="attribute">
+        <span className="label">Araç Aktarımı:</span>
+        <span className="value">vehicle_transfer</span>
+      </div>
+    </div>
 
     <div className='main-car_features'>Araç Özellikleri</div>
     <ul className='main-car_features_texts'>
@@ -86,19 +115,20 @@ function otoDetails() {
       <li>Features</li>
       <li>Features</li>
       <li>Features</li>
+      <li>Features</li>
+      <li>Features</li>
+      <li>Features</li>
     </ul>
 
-      {/* --------FOOTER SECTION-------- */}
+    {/* --------FOOTER-------- */}
       <footer className='footer'>
         <div className='footer_menu'>
-          <ul className='footer_menu-item'>Yerimiz</ul>
-          <ul className='footer_menu-item'>Araçlarımız</ul>
-          <ul className='footer_menu-item'>İletişim</ul>
+          <a href='#'><ul className='footer_menu-item'>Yerimiz</ul></a>
+          <a href='#'><ul className='footer_menu-item'>Araçlarımız</ul></a>
+          <a href='#'><ul className='footer_menu-item'>İletişim</ul></a>
         </div>
         <div className='footer_icons'>
-          <ul className='footer_menu-instagram'>
-          <InstagramLogo/>
-          </ul>
+          <a href='#'><ul className='footer_menu-instagram'><InstagramLogo/></ul></a>
         </div> 
         <div className='footer_company-text'>Kibrisarabam @ 2024 All rights reserved.</div>
       </footer>
